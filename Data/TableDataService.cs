8902f6201c13382ec2c774d5f23b3f176fd823a5
Dale <djs93@njit.edu>
2022-03-22 21:29:35 -0400

    Add Songs list and get and async set methods

--- Data/TableDataService.cs
+++ Data/TableDataService.cs
@@ -2,5 +2,16 @@
 
 public class TableDataService
 {
+    private static List<Song> Songs = new List<Song>();
     
+    public Task<List<Song>> GetSongsAsync()
+    {
+        return Task.FromResult(Songs);
+    }
+
+    public Task<List<Song>> SetSongsAsync(List<Song> songs)
+    {
+        Songs = songs;
+        return Task.FromResult(Songs);
+    }
 }

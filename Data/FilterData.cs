a459fbffc3130f0f0c8ef289eb861a9d6e081b47
Dale <djs93@njit.edu>
2022-04-05 21:13:01 -0400

    Create filter data class

---
+++ Data/FilterData.cs
@@ -0,0 +1,9 @@
+﻿namespace playlistimport_blazor.Data;
+
+public class FilterData
+{
+    public string? Artist { get; set; }
+    public int Year { get; set; }
+    public string? Genre { get; set; }
+    public int NumberOfSongs { get; set; }
+}
